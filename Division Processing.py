159e798049c065e7cdacd570cfee31277f12ca92
ju hee Lee <stella9605@naver.com>
2020-06-11 01:19:36 +0900

    Update Division Processing.py

--- Division Processing.py
+++ Division Processing.py
@@ -3,7 +3,7 @@ import numpy as np
 %matplotlib inline
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+run profile1
 
 from sklearn.multioutput import MultiOutputRegressor
 import optuna
@@ -22,11 +22,13 @@ import warnings ; warnings.filterwarnings('ignore')
 import time
 from sklearn.metrics import f1_score, roc_auc_score, classification_report
 
+from sklearn.metrics import mean_absolute_error
 
 train = pd.read_csv('train.csv', index_col = 'id')
 test = pd.read_csv('test.csv', index_col = 'id')
 submission = pd.read_csv('sample_submission.csv', index_col='id')
 
+mean(is.na(train))
 print(train.shape, test.shape, submission.shape)
 # (10000, 75) (10000, 71) (10000, 4)
 
@@ -54,6 +56,14 @@ params={'max_depth':10, 'learning_rate':0.07, 'num_iterations':200,
 
 multi_model = MultiOutputRegressor(params)
 
+y_train.values.i
+
+
 ## - ing
 # k-fold cv로 모델 평가해보기, 
 
+# y를 분할했으니까 neural network 에 각각 형태에 따라 서로다른 형태로 가공하기위해 더미변수가 필요하다
+# 설명변수는 더미데이터로 들어갈 필요가없
+# 1. 결측치 주의
+# 2. 인과관계 역으로 흘러가지않게 파악(x가 원인, y가 결과, y의 결과가 x에 미치지않게)
+# 3. 수치(비율조절 조심)
