11b686e43670ed792563cc3a856187a736c55405
ju hee Lee <stella9605@naver.com>
2020-06-11 17:23:53 +0900

    heatmap

--- Division Processing.py
+++ Division Processing.py
@@ -24,39 +24,78 @@ from sklearn.metrics import f1_score, roc_auc_score, classification_report
 
 from sklearn.metrics import mean_absolute_error
 
-train = pd.read_csv('train.csv', index_col = 'id')
-test = pd.read_csv('test.csv', index_col = 'id')
+train = pd.read_csv('train.csv', index_col='id')
+test = pd.read_csv('test.csv', index_col='id')
 submission = pd.read_csv('sample_submission.csv', index_col='id')
 
-mean(is.na(train))
+
 print(train.shape, test.shape, submission.shape)
-# (10000, 75) (10000, 71) (10000, 4)
+# (10000, 76) (10000, 72) (10000, 6)
 
 submission.head
 train.head
 
+# 결측치 처리 
+train.isna().sum().plot
+test.isna().sum().plot
+plt.show()
+
+
+test = test.fillna(train.mean())
+train = train.fillna(train.mean())
+
+train.head
+
+# train test 분리 
 
 feature_names = list(test)
 target_names=list(submission)
+
 
 x_train = train[feature_names]
 x_test = test[feature_names]
 
 y_train = train[target_names]
+
+# t_test 대신 매개변수를 분리하여 개별측정 
 y_train_hhb = y_train['hhb'] 
 y_train_hbo2 = y_train['hbo2']
 y_train_ca = y_train['ca']
 y_train_na = y_train['na']
+
+
 
 #parameters
 d_train = lgb.Dataset(x_train, label=y_train)
 params={'max_depth':10, 'learning_rate':0.07, 'num_iterations':200,
         'boosting':'gbdt','sub_feature':0.5, 'feature_fraction':0.7, 'objective':11,
         'num_leaves':90}
-#[Ref. http://machinelearningkorea.com/2019/09/29/lightgbm-%ED%8C%8C%EB%9D%BC%EB%AF%B8%ED%84%B0/] 
-multi_model = MultiOutputRegressor(params)
 
-y_train.values.i
+
+base_params={'n_estimators': 900, 'num_leaves': 90, 'learning_rate': 0.1, 
+             'colsample_bytree': 0.8, 'subsample': 0.9, 
+             'reg_alpha': 5, 'reg_lambda': 7}
+
+#[Ref. http://machinelearningkorea.com/2019/09/29/lightgbm-%ED%8C%8C%EB%9D%BC%EB%AF%B8%ED%84%B0/] 
+
+base_model=LGBMRegressor(objective='l1', subsample_freq=1, silent=False, random_state=18, 
+                         importance_type='gain', **base_params)
+
+model=MultiOutputRegressor(base_model)
+
+model.fit(x_train, y_train)
+
+preds = model.predict(x_test)
+submission.columns
+md = DataFrame(preds, columns = ['hhb','hbo2','ca','na'])
+dataframe = pd.DataFrame(md)
+
+dataframe = dataframe.fillna(0)
+
+dataframe.to_csv('predstest.csv')
+
+          
+          y_train.values.i
 
 
 ## - ing
@@ -67,3 +106,6 @@ y_train.values.i
 # 1. 결측치 주의
 # 2. 인과관계 역으로 흘러가지않게 파악(x가 원인, y가 결과, y의 결과가 x에 미치지않게)
 # 3. 수치(비율조절 조심)
+# xgboost
+# xgb = XGBClassifier(n_estimators=500, learning_rate=0.1, max_depth=4)
+# xgb.fit(x_train, y_train)
--- Processing2.py
+++ Processing2.py
@@ -6,13 +6,15 @@ import matplotlib.pyplot as plt
 
 import xgboost as xgb
 from sklearn.model_selection import KFold
+from xgboost import plot_importance
+from xgboost import XGBClassifier
 
 import warnings
 warnings.filterwarnings(action='ignore')
 
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
-submission=pd.read_csv('sample_submission.csv')
+train = pd.read_csv('train.csv', index_col ='id')
+test = pd.read_csv('test.csv', index_col = 'id')
+submission=pd.read_csv('sample_submission.csv', index_col = 'id')
 
 train.head()  # (5 x 76)
 test.head()   # (5 x 72)
@@ -29,12 +31,67 @@ train = train.fillna(train.mean())
 
 # 데이터와 hho, hbo2, ca, na 상관관계 분석
 plt.figure(figsize=(4,12))
+sns.heatmap(train.corr().loc['rho':'990_src','hhb':].abs())
+sns.heatmap(train.corr().loc['650_dst':'990_dst','hhb':].abs())
 sns.heatmap(train.corr().loc['rho':'990_dst','hhb':].abs())
 
-x_train = train.loc[:,'650_dst':'990_dst']
-y_train = train.loc[:,'hhb':'na']
-x_train.shape # (10000,35)
-y_train.shape # (10000,4)
+
+
+X = train.loc[:,'rho':'990_dst']
+Y = train.loc[:,'hhb':'na']
+
+x_train, y_train, x_test, y_test = train_test_split(X,
+                                                    Y,
+                                                    random_state=0)
+print(x_train.shape, x_test.shape)
+# (7500, 71) (7500, 4)
+
+xgb = XGBClassifier(n_estimators=500,learning_rate = 0.1, max_depth=4 )
+xgb.fit(x_train,y_train)
+xgb.predict(x_test)
+
+
 
 # xgboost 로 kfold, 
 # train set 분리하고 params로 lightgbm 사용할것 
+
+
+# 흡광도 
+# 계산식 (흡광도 계산식)
+# A(흡광도) = -log10(I(투과방사선)/I0(입사방사선))  
+#           = ε(흡광계수) ⋅ b(투과 경로 길이(cm)) ⋅ c(농도)
+train.head
+
+# 튜닝 함수
+def tuning_var(s):
+    s_rho = s[0]          # _rho
+    s_src = s[1:36]       # _src
+    s_dst = s[36:]        # _dst    
+
+    # index 표준화
+    set_index = s_src.index.str.split('_').str[0]
+    s_src.index = set_index
+    s_dst.index = set_index
+
+    # 계산식 (흡광도 계산식)
+    # A(흡광도) = -log10(I(투과방사선)/I0(입사방사선))  
+    #           = ε(흡광계수) ⋅ b(투과 경로 길이(cm)) ⋅ c(농도)
+    s_ds_st = ((s_dst / s_src) / (s_rho/10))
+    
+    # 계산 완료후 inf,nan 0으로 치환
+    s_ds_st = [i if i != np.inf else 0.0 for i in s_ds_st ]
+    s_ds_st = Series(s_ds_st).fillna(value = 0)
+    
+    # math.log 계산을 위해 0을 1로 치환후 계산(흡광계수는 1로 가정한다.)
+    s_ds_st = [1 if i == 0 else i for i in s_ds_st ]
+    
+    # 변수 튜닝 반환
+    out_s = Series(map(lambda x : -math.log(x,10), s_ds_st))
+    out_s.index= set_index
+    return(out_s)
+
+tunning_train_x = train_x.apply(tuning_var, axis = 1)
+
+
+# xgboost
+
